567f0df7663ce1e964b55d5257feff38c366180d
Giorgi Kvatsabaia <kvacabaya02@gmail.com>
2025-04-04 00:07:45 +0400

    Initial commit

---
+++ src/main/java/build/master/BuildMasterApplication.java
@@ -0,0 +1,13 @@
+package build.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/build/master/BuildMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package build.master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
